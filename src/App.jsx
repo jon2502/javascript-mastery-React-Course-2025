31a91866e0ec564796fbca835c27529f63fb3563
jon2502 <jonbjarke25@outlook.dk>
2025-02-27 17:45:31 +0100

    finished setup of tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-
+const App = () =>{
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
+    </div>
+  )
+}
 
 
 export default App
