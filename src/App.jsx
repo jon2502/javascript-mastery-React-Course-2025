5ff7239e12e40ff4b358356f91b159f8c70eedd8
jon2502 <jonbjarke25@outlook.dk>
2025-02-27 16:13:03 +0100

    setup of props

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Card = ({title}) => {
+  return (
+  <div>
+    <h2>{title}</h2>
+  </div>
+  )
+}
+
 const App = () => {
   return (
-    <h2>functional arrow function</h2>
+    <div>
+      <h2>functional arrow function</h2>
+      <Card title="Godzilla"/>
+      <Card title="Pacific rim"/>
+      <Card title="Gamera"/>
+    </div>
+
   )
 }
 
