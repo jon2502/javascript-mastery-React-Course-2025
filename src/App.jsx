d6fb54ea0536e9f60d27626f8b17bd753f4f459f
jon2502 <jonbjarke25@outlook.dk>
2025-02-27 17:12:41 +0100

    clearup for setup of movie application

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,5 @@ import './App.css'
 
 
 
-const Card = ({title}) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-  
-
-  useEffect(()=>{
-    console.log(`${title} has been liked: ${hasLiked}`)
-  }, [hasLiked]);
-
-
-
-  return (
-  <div className="card" onClick={() => setCount(count + 1)}>
-    <h2>{title} <br/> {count || null}</h2>
-
-    <button onClick={()=> setHasLiked(!hasLiked)}>
-     {hasLiked ? "Liked" : "Like"}
-    </button>
-  </div>
-  )
-}
-
-const App = () => {
-  
-
-  return (
-    <div className='card-container'>
-      <Card title="Godzilla"/>
-      <Card title="Pacific rim"/>
-      <Card title="Gamera"/>
-    </div>
-
-  )
-}
 
 export default App
