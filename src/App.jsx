911d78e898a4577985139d331657c8689a191549
jon2502 <jonbjarke25@outlook.dk>
2025-02-27 18:28:04 +0100

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const App = () =>{
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-          Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> you'll Enjoy Without the Hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   )
 }
 
