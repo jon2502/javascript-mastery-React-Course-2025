9592a2b9fc96a53988f41d75e11c959b9bae2ac8
jon2502 <jonbjarke25@outlook.dk>
2025-02-27 17:08:45 +0100

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ const Card = ({title}) => {
     console.log(`${title} has been liked: ${hasLiked}`)
   }, [hasLiked]);
 
+
+
   return (
   <div className="card" onClick={() => setCount(count + 1)}>
-    <h2>{title} <br/> {count}</h2>
+    <h2>{title} <br/> {count || null}</h2>
 
     <button onClick={()=> setHasLiked(!hasLiked)}>
      {hasLiked ? "Liked" : "Like"}
