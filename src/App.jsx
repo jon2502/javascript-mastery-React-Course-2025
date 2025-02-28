bedb5962629c991a39b946c676efeffb3241f1fe
jon2502 <jonbjarke25@outlook.dk>
2025-02-28 14:20:32 +0100

    setup appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_ACCESS_TOKEN;
---
+++ src/appwrite.js
@@ -0,0 +1,7 @@
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID
+const COLLECTIO_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID
+
+export const updateSearchCount = async () =>{
+    
+}
