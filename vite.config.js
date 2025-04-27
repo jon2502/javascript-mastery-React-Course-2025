0a934b2e7ab64215615091c615c61f70a8fb99a9
jon2502 <jonbjarke25@outlook.dk>
2025-04-27 17:58:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base:"/javascript-mastery-React-Course-2025/",
+  base:"/javascript-mastery-React-Course-2025",
 })
