cc33f8a3770b616ab4ac074f866ccbdf5501cad3
jon2502 <jonbjarke25@outlook.dk>
2025-06-16 20:01:00 +0200

    Update SelectedMovie.jsx

--- src/components/SelectedMovie.jsx
+++ src/components/SelectedMovie.jsx
@@ -17,22 +17,26 @@ const SelectedMovie =({isMovieID, setisMovieID, Movie, setselectedMovie}) => {
                 setisMovieID(false) 
                 setselectedMovie(false)
                 }}>back</button>
-            <div className="bg-cover bg-no-repeat"
-            style={
+            <section>
+                <div className="bg-cover bg-no-repeat"
+                style={
                 {backgroundImage: `linear-gradient(to right, rgba(220.5, 241.5, 241.5, 1) calc((50vw - 170px) - 340px), rgba(220.5, 241.5, 241.5, 0.84) 50%, rgba(220.5, 241.5, 241.5, 0.84) 100%), url(https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces${Movie.backdrop_path})`}
-            }>
-                <img src={Movie.poster_path ?
-                `https://image.tmdb.org/t/p/w500/${Movie.poster_path}`: './no-movie.png'}
-                alt={Movie.title}/>
-                <div className=''>
-                    <h2>{Movie.title} ({Movie.release_date ? Movie.release_date.split('-')[0] : 'N/A'})</h2>
-                    <p>{Movie.release_date} ({Movie.origin_country[0]})</p>
-                    <span>{Movie.genres.map((genre)=>(<a key={genre.name}>{genre.name}, </a>))}</span> 
-                    <p>{HouresandMinutes(Movie.runtime)}</p>
+                }>
+                    <img src={Movie.poster_path ?
+                    `https://image.tmdb.org/t/p/w500/${Movie.poster_path}`: './no-movie.png'}
+                    alt={Movie.title}/>
+                    <div className=''>
+                        <h2>{Movie.title} ({Movie.release_date ? Movie.release_date.split('-')[0] : 'N/A'})</h2>
+                        <div className='flex'>
+                            <p>{Movie.release_date} ({Movie.origin_country[0]})</p>
+                            <span>{Movie.genres.map((genre)=>(<a key={genre.name}>{genre.name}, </a>))}</span> 
+                            <p>{HouresandMinutes(Movie.runtime)}</p>
+                        </div>
+                    </div>
+                    <h2>overview</h2>
+                    <p>{Movie.overview}</p>
                 </div>
-                <h2>overview</h2>
-                <p>{Movie.overview}</p>
-            </div>
+            </section>
         </div>
     )
 }
